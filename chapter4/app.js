2dce3fad19c1b3f6cc0fe594734117e70def798d
nicolasoarantes <nicolasoarantes@gmail.com>
2023-05-25 17:27:41 -0300

    First commit-  exmaple

---
+++ chapter4/app.js
@@ -0,0 +1,7 @@
+var main = function () {
+    "use strict";
+    // ^disallows certain bad aspects of JavaScript that have caused problems for programmers in the past.
+    //  This will always be the first line in our main function.
+    window.alert("hello world!");
+    };
+    $(document).ready(main);
